1ba09035d7aa9b832c8adb66c9afe923c5b511ba
MoYingJi <moyingjiaw@outlook.com>
2025-02-16 02:24:57 +0800

    在修了在修了（

--- docs/.vitepress/theme/cursor/cursor.ts
+++ docs/.vitepress/theme/cursor/cursor.ts
@@ -1,40 +1,41 @@
-console.log("Custom Cursor Loading!")
-
 import './cursor.sass'
 
-const cr = document.createElement('div')
-cr.classList.add('cursor-ring') // 添加类名以便应用样式
-document.body.appendChild(cr)
+if (!import.meta.env.SSR) {
+    console.log("Custom Cursor Loading!")
 
-let tx = 0
-let ty = 0
-let cx = 0
-let cy = 0
+    const cr = document.createElement('div')
+    cr.classList.add('cursor-ring') // 添加类名以便应用样式
+    document.body.appendChild(cr)
 
-document.addEventListener('mousemove', (e) => {
-    tx = e.clientX
-    ty = e.clientY
-})
+    let tx = 0
+    let ty = 0
+    let cx = 0
+    let cy = 0
 
-document.addEventListener('mouseover', mouseStyleChange)
-document.addEventListener('mouseout',  mouseStyleChange)
+    document.addEventListener('mousemove', (e) => {
+        tx = e.clientX
+        ty = e.clientY
+    })
+    document.addEventListener('mouseover', mouseStyleChange)
+    document.addEventListener('mouseout',  mouseStyleChange)
 
-function mouseStyleChange(e: MouseEvent) {
-    if (e.target instanceof HTMLElement) {
-        const t = e.target
-        const s = window.getComputedStyle(t)
-        const c = s.getPropertyValue("--custom-cursor-ring")
-        if (c === "active") cr.classList.add('active')
-        else cr.classList.remove('active')
+    function mouseStyleChange(e: MouseEvent) {
+        if (e.target instanceof HTMLElement) {
+            const t = e.target
+            const s = window.getComputedStyle(t)
+            const c = s.getPropertyValue("--custom-cursor-ring")
+            if (c === "active") cr.classList.add('active')
+            else cr.classList.remove('active')
+        }
     }
-}
 
-function animate() {
-    cx += (tx - cx) * 0.35
-    cy += (ty - cy) * 0.35
-    cr.style.left = `${cx}px`
-    cr.style.top  = `${cy}px`
+    function animate() {
+        cx += (tx - cx) * 0.35
+        cy += (ty - cy) * 0.35
+        cr.style.left = `${cx}px`
+        cr.style.top  = `${cy}px`
 
-    requestAnimationFrame(animate)
+        requestAnimationFrame(animate)
+    }
+    animate()
 }
-animate()
